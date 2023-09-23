628973235344d404d4d5c8d76f3afad5e3645759
Nap Joseph Calub <njncalub@gmail.com>
2023-09-23 17:02:07 +0800

    feat: add initial endpoints

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,13 +4,33 @@
 
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/index.tsx";
+import * as $2 from "./routes/api/auth/login.ts";
+import * as $3 from "./routes/api/movies/[movieId]/copies/[copyId].ts";
+import * as $4 from "./routes/api/movies/[movieId]/copies/index.ts";
+import * as $5 from "./routes/api/movies/[movieId]/index.ts";
+import * as $6 from "./routes/api/movies/index.ts";
+import * as $7 from "./routes/api/rentals/[rentalId].ts";
+import * as $8 from "./routes/api/rentals/index.ts";
+import * as $9 from "./routes/api/users/[userId]/index.ts";
+import * as $10 from "./routes/api/users/[userId]/rentals.ts";
+import * as $11 from "./routes/api/users/index.ts";
+import * as $12 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_app.tsx": $1,
-    "./routes/index.tsx": $2,
+    "./routes/api/auth/login.ts": $2,
+    "./routes/api/movies/[movieId]/copies/[copyId].ts": $3,
+    "./routes/api/movies/[movieId]/copies/index.ts": $4,
+    "./routes/api/movies/[movieId]/index.ts": $5,
+    "./routes/api/movies/index.ts": $6,
+    "./routes/api/rentals/[rentalId].ts": $7,
+    "./routes/api/rentals/index.ts": $8,
+    "./routes/api/users/[userId]/index.ts": $9,
+    "./routes/api/users/[userId]/rentals.ts": $10,
+    "./routes/api/users/index.ts": $11,
+    "./routes/index.tsx": $12,
   },
   islands: {},
   baseUrl: import.meta.url,
